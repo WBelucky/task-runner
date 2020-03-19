2dcb32fed20887dfe6d1f25db10e05a2f20aa7bc
WBelucky <shintaro.kusabiraki@gmail.com>
2020-03-19 18:53:38 +0900

    Add reset

--- src/App.tsx
+++ src/App.tsx
@@ -3,34 +3,58 @@ import './App.css';
 import Button from '@material-ui/core/Button'
 import { Typography } from '@material-ui/core';
 
-const useTimer: () => [boolean, React.Dispatch<React.SetStateAction<boolean>>, number] = () => {
+const useTimer = () => {
   const [timerRunning, setTimerRunning] = useState(false);
   const [elapsedMs, setElapsedMs] = useState(0);
+  const [startMs, setStartMs] = useState(0)
+  const [isPosed, setIsPosed] = useState(false);
 
-  const tick = (start: number) => setElapsedMs(new Date().getTime() - start);
+  const startTimer = () => {
+    setStartMs(Date.now() - (isPosed ? elapsedMs : 0));
+    setTimerRunning(true);
+  }
+  const stopTimer = () => {
+    setIsPosed(true);
+    setTimerRunning(false);
+  }
+  const resetTimer = () => {
+    setIsPosed(false);
+    setTimerRunning(false);
+    setElapsedMs(0);
+  }
+
   useEffect( () => {
     if (!timerRunning) return;
-    const start = new Date().getTime();
+
+    const tick = () => setElapsedMs(Date.now() - startMs);
     const timerID = setInterval(
-      () => tick(start), 1000
+      () => tick(), 1000
     )
     return () => clearInterval(timerID)
   }, [timerRunning]);
-  return [timerRunning, setTimerRunning, elapsedMs];
-}
 
-const useCountDown = () => {
-  
+  return {
+    timerRunning,
+    startTimer,
+    stopTimer,
+    resetTimer,
+    elapsedMs
+  } as const;
 }
 
 function App() {
-  const [timerRunning, setTimerRunning, elapsedMs] = useTimer();
+  // const [timerRunning, setTimerRunning, elapsedMs] = useTimer();
+  const { timerRunning, startTimer, stopTimer, resetTimer, elapsedMs } = useTimer();
+
   return (
     <div className="App">
       <header className="App-header">
         <Typography>{Math.floor(elapsedMs / 1000)}</Typography>
-        <Button onClick={ () => setTimerRunning(!timerRunning)}>
+        <Button onClick={ () => (timerRunning ? stopTimer() : startTimer()) }>
           { timerRunning ? "Stop" : "Start"}
+        </Button>
+        <Button onClick={ () => resetTimer() }>
+          Reset
         </Button>
       </header>
     </div>
