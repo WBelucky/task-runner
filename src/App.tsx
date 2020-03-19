9c3c4f647690378d4327136432bf2943069c0278
WBelucky <shintaro.kusabiraki@gmail.com>
2020-03-20 02:11:38 +0900

    replace tslint with typescript-eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,59 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import Button from '@material-ui/core/Button'
-import { Typography } from '@material-ui/core';
+import React, { useState, useEffect } from 'react'
+import './App.css'
+import 'semantic-ui-css/semantic.min.css'
+import { Button, Grid } from 'semantic-ui-react'
 
-const useStopwatch= () => {
-  const [isRunning, setIsRunning] = useState(false);
-  const [isPosed, setIsPosed] = useState(false);
-  const [elapsedMs, setElapsedMs] = useState(0);
+const useStopwatch = () => {
+  const [isRunning, setIsRunning] = useState(false)
+  const [isPosed, setIsPosed] = useState(false)
+  const [elapsedMs, setElapsedMs] = useState(0)
   const [startMs, setStartMs] = useState(0)
 
-  useEffect( () => {
-    if (!isRunning) return;
+  useEffect(() => {
+    if (!isRunning) return
 
-    const tick = () => setElapsedMs(Date.now() - startMs);
+    const tick = () => setElapsedMs(Date.now() - startMs)
     const timerID = setInterval(
       () => tick(), 1000
     )
     return () => clearInterval(timerID)
-  }, [isRunning, startMs]);
+  }, [isRunning, startMs])
 
   return {
     isRunning,
     elapsedMs,
     start: () => {
-      setStartMs(Date.now() - (isPosed ? elapsedMs : 0));
-      setIsRunning(true);
+      setStartMs(Date.now() - (isPosed ? elapsedMs : 0))
+      setIsRunning(true)
     },
     stop: () => {
-      setIsPosed(true);
-      setIsRunning(false);
+      setIsPosed(true)
+      setIsRunning(false)
     },
     reset: () => {
-      setIsPosed(false);
-      setIsRunning(false);
-      setElapsedMs(0);
+      setIsPosed(false)
+      setIsRunning(false)
+      setElapsedMs(0)
     }
-  } as const;
+  } as const
 }
 
-
 const useCountDown = (remaining: number) => {
-  const { isRunning, start, stop, reset, elapsedMs } = useStopwatch();
-  const [ initialMs, setInitialMs ] = useState(remaining);
-  const [ remainingMs, _setRemainingMs ] = useState(remaining);
+  const { isRunning, start, stop, reset, elapsedMs } = useStopwatch()
+  const [initialMs, setInitialMs] = useState(remaining)
+  const [remainingMs, _setRemainingMs] = useState(remaining)
   useEffect(() => {
-    if ( initialMs - elapsedMs <= 0 ) {
-      console.log('complete');
+    if (initialMs - elapsedMs <= 0) {
+      console.log('complete')
       stop()
       // onCompleteって感じにしたい.
     }
     _setRemainingMs(Math.max(initialMs - elapsedMs, 0))
-  }, [elapsedMs])
+  }, [elapsedMs, initialMs, stop])
 
   const setRemainingMs = (ms: number) => {
-    setInitialMs(ms);
-    _setRemainingMs(ms);
+    setInitialMs(ms)
+    _setRemainingMs(ms)
   }
   return {
     remainingMs,
@@ -63,33 +62,34 @@ const useCountDown = (remaining: number) => {
     setRemainingMs,
     reset,
     stop
-  } as const;
+  } as const
 }
 
 const toString = (ms: number) => {
-  const h = Math.floor(ms / 1000 / 60 / 60);
-  const m = Math.floor(ms / 1000 / 60 - h * 60);
-  const s = Math.ceil(ms / 1000 - m * 60 );
-  return `${h > 0 ? h + ' : ' : ''}${('00'+ m).slice(-2)} ' ${('00'+ s).slice(-2)}`;
+  const h = Math.floor(ms / 1000 / 60 / 60)
+  const m = Math.floor(ms / 1000 / 60 - h * 60)
+  const s = Math.ceil(ms / 1000 - m * 60)
+  return `${h > 0 ? h + ' : ' : ''}${('00' + m).slice(-2)} '${('00' + s).slice(-2)}`
 }
 
-function App() {
-
-  const { isRunning, start, stop, reset, remainingMs } = useCountDown(1000 * 60 * 25);
+function App () {
+  const { isRunning, start, stop, reset, remainingMs } = useCountDown(1000 * 60 * 25)
 
   return (
     <div className="App">
       <header className="App-header">
-        <Typography>{toString(remainingMs)}</Typography>
-        <Button onClick={(isRunning ? stop: start) }>
-          { isRunning ? "Stop" : "Start" }
-        </Button>
-        <Button onClick={reset}>
-          Reset
-        </Button>
+        <p>{toString(remainingMs)}</p>
+        <Grid>
+          <Button onClick={(isRunning ? stop : start)}>
+            {isRunning ? 'Stop' : 'Start'}
+          </Button>
+          <Button onClick={reset}>
+            Reset
+          </Button>
+        </Grid>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
